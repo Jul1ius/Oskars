b9edef1ec507ea08b707ee15ef57f2eccb28bf43
its julii <nanji828@gmail.com>
2022-12-10 10:20:25 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Oskars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           int naem = int.Parse(Console.ReadLine());
+
+            double statuetki = naem * 0.7;
+            double ketaring = statuetki * 0.85;
+            double ozvuchavane = ketaring / 2;
+
+            double totalsum = naem + statuetki + ozvuchavane + ketaring;
+            Console.WriteLine($"{totalsum:f2}");  
+        }
+    }
+}
